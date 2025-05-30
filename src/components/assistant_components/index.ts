120b23a84bbdf659e00e9d996fa143ec0a7f6b07
aruntemme <arungauthamk@gmail.com>
2025-05-30 20:42:46 +0530

    chore: remove duplicate files and deprecated

--- src/components/assistant_components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AssistantHeader } from './AssistantHeader';
-export { default as ChatInput } from './ChatInput';
-export { default as ChatMessage } from './ChatMessage';
-export { default as ChatWindow } from './ChatWindow';
-export { default as KnowledgeBaseModal } from './KnowledgeBaseModal';
-export { default as ModelConfigModal } from './ModelConfigModal';
-export { default as OnboardingModal } from './OnboardingModal';
-export { ToolModal } from './ToolModal';
